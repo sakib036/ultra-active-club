122e01a44ab83995ede23b08547e00a738298050
Md. Nazmus Sakib <sakib036@gmail.com>
2022-09-29 15:39:30 +0600

    local-store

--- src/component/ExerciseDetail/ExerciseDetail.js
+++ src/component/ExerciseDetail/ExerciseDetail.js
@@ -1,16 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './ExerciseDetail.css'
 import myPic from '../../images/Nazmus Sakib.jpg'
 import Break from '../Break/Break';
 import BodyDetail from '../BodyDetail/BodyDetail';
 import Time from '../Time/Time';
 
+
+
 const ExerciseDetail = ({time}) => {
     const [rest,setRest]=useState(0);
     const breakTime=(breakTime)=>{
+       
     const rest=(breakTime);
+    localStorage.setItem('restTime', JSON.stringify(rest));
     setRest(rest);   
     }
+    useEffect(() => {
+        const rest = JSON.parse(localStorage.getItem('restTime'));
+        if (rest) {
+         setRest(rest);
+        }
+      }, []);
    
     return (
         <div className='exercise-details'>
